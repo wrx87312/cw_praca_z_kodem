7b8f1138247d1d2320d14400f809b7cf1a80569b
wrx87312 <wrx87312@student.wroclaw.merito.pl>
2024-03-26 15:42:08 +0100

    automatyzacja w IT cw 2

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
-from markupsafe import escape
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', name="Dawid")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
